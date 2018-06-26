3b5fea3e6de628de334c903bc60e1349d6231ee1
UstehKenny <carosiim.sdco@gmail.com>
2018-06-26 00:36:53 -0500

    Tarea Archivos
    El programa genera archivos cada que se logra ingresar.

--- Ejercicios en clase /registroLogin.py
+++ Ejercicios en clase /registroLogin.py
@@ -1,6 +1,5 @@
 usuarios = {} #Creamos un diccionario en donde, como llave, guardara el nombre del usuario y ,como valor, guardara la informacion del usuario que se pedira a continuacion
 #Creamos la lista en donde se van a guardar los datos del usuario
-#listaUsuarios = []
 unaLista = []
 
 #Menu principal: Pide al usuario seleccionar una opcion y dependiendo de esta, va a llevar a dos funciones
@@ -30,6 +29,7 @@ def Login(usuarios):
     if user in usuarios:
         if  usuarios[user][0] == passw: #Si el usuario y contrasenia son correctos, dejara entrar al usuario
             print("Bienvenido")
+            generaArchivo(user)
         else:
             print("Contrasenia incorrecta") #Si el usuario es correcto pero la contrasenia es incorrecta, no dejara loggearse
     else: #Si el usuario no coincide en el diccionario, imprimira lo siguiente:
@@ -38,29 +38,29 @@ def Login(usuarios):
 def Registro():
         username = input("Nombre de usuario: ") #Se pide el nombre de usuario
         password = input("Contrasenia: ") #Se pide una contrasenia
-        unaLista.append(password)
-        #listaUsuarios.append(password)
+        unaLista.append(password) #0
+        
         #Aqui definimos la llave y el valor del diccionario de usuarios
         nombre = input("Ingresa nombre: ")
-        unaLista.append(nombre)
-        #listaUsuarios.append(nombre)
+        unaLista.append(nombre) #1
+        
         apellido = input("Ingresa apellido: ")
-        unaLista.append(apellido)
-        #listaUsuarios.append(apellido)
+        unaLista.append(apellido) #2
+       
         edad = input("Ingresa edad: ")
-        unaLista.append(edad)
-        #listaUsuarios.append(edad)
+        unaLista.append(edad) #3
+        
         numTarj = input("Ingresa numero de tarjeta: ")
-        unaLista.append(numTarj)
-        #listaUsuarios.append(numTarj)
+        unaLista.append(numTarj) #4
+        
         pp = []
         paypalCorreo = input("Ingresa correo de PayPal: ")
-        pp.append(paypalCorreo)
+        pp.append(paypalCorreo) #5.0
         paypalContrasenia = input("Ingresa contrasenia de PayPal")
-        pp.append(paypalContrasenia)
+        pp.append(paypalContrasenia) #5.1
 
-        unaLista.append(pp)
-        #listaUsuarios = listaUsuarios.append(pp)
+        unaLista.append(pp) #5
+
         usuarios[username] = unaLista
 
         respuesta = input("Deseas hacer otro registro? s/n ")
@@ -71,15 +71,17 @@ def Registro():
         else:
             print("Opcion invalida")
 
-        #return usuarios
-"""
+
 def generaArchivo(user):
-    archivo = open('registro.txt','w')
-    nombre = dicc[user[1]]
-    apellido = dicc[user[2]]
-    edad = dicc[user[3]]
-    archivo.write("Bienvenido: "+ nombre + apellido)
-    archivo.write("Edad: " + edad)
-    archivo.write("Numero de tarjeta: ") """
+    archivo = open('BienvenidoUsuario.txt','w')
+    nombre = usuarios[user][1]
+    apellido = usuarios[user][2]
+    edad = usuarios[user][3]
+    tarjeta = usuarios[user][4]
+    payp = usuarios[user][5][0]
+    archivo.write("Bienvenido: "+ nombre + " " +apellido)
+    archivo.write("\nEdad: " + edad)
+    archivo.write("\nNumero de tarjeta: " + tarjeta)
+    archivo.write("\nCuenta de PayPal: " + payp)
 
 Menu()
