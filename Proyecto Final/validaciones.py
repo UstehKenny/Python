68a2f070673166392eda8a4548336402cdb0dc92
UstehKenny <carosiim.sdco@gmail.com>
2018-07-22 20:25:33 -0500

    Prueba
    Prueba de validaciones para la contraseña y usuarios

---
+++ Proyecto Final/validaciones.py
@@ -0,0 +1,58 @@
+"""import getpass
+passw = input("Ingresa la contrasenia: ")
+if len(passw) >= 8:
+    print("Es mayor a 8")
+    if passw.isupper() == False:
+        print("Tiene mayusculas")
+        if passw.islower() == False:
+            print("Tiene minusculas")
+            if passw.isdigit() == False:
+                print("Tiene numero")
+                if passw.isalnum() == False:
+                    print("Tiene un caracter")
+                    if passw.isspace() == False:
+                        print("No tiene espacios\nCONTRASENIA VALIDA")
+                    else:
+                        print("Tiene espacios\nCONTRASENIA INVALIDA")
+                else:
+                    print("No tiene caracteres")
+            else:
+                print("No tiene numero")
+        else:
+            print("No tiene minusculas")
+    else:
+        print("No tiene mayusculas")
+else:
+    print("Menor a 8")
+
+"""
+usuarios = {}
+unaLista = []
+username = input("Nombre de usuario: ") #Se pide el nombre de usuario
+password = input("Contrasenia: ") #Se pide una contrasenia
+unaLista.append(password) #0
+        
+        #Aqui definimos la llave y el valor del diccionario de usuarios
+nombre = input("Ingresa nombre: ")
+unaLista.append(nombre) #1
+        
+apellido = input("Ingresa apellido: ")
+unaLista.append(apellido) #2
+       
+edad = input("Ingresa edad: ")
+unaLista.append(edad) #3
+        
+numTarj = input("Ingresa numero de tarjeta: ")
+unaLista.append(numTarj) #4
+        
+pp = []
+paypalCorreo = input("Ingresa correo de PayPal: ")
+pp.append(paypalCorreo) #5.0
+paypalContrasenia = input("Ingresa contrasenia de PayPal")
+pp.append(paypalContrasenia) #5.1
+
+unaLista.append(pp) #5
+usuarios[username] = unaLista
+
+llaves =usuarios.keys()
+print("Claves: %s " %llaves)
