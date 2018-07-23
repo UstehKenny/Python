a2256b4f2eded0d929ff558b512cbc61155879f8
UstehKenny <carosiim.sdco@gmail.com>
2018-07-22 21:41:45 -0500

    Prueba para visualizar los articulos
    xd

--- Proyecto Final/validaciones.py
+++ Proyecto Final/validaciones.py
@@ -1,4 +1,6 @@
-"""import getpass
+"""
+Codigo para pruebas
+import getpass
 passw = input("Ingresa la contrasenia: ")
 if len(passw) >= 8:
     print("Es mayor a 8")
@@ -28,31 +30,35 @@ else:
 """
 usuarios = {}
 unaLista = []
-username = input("Nombre de usuario: ") #Se pide el nombre de usuario
-password = input("Contrasenia: ") #Se pide una contrasenia
-unaLista.append(password) #0
         
-        #Aqui definimos la llave y el valor del diccionario de usuarios
-nombre = input("Ingresa nombre: ")
-unaLista.append(nombre) #1
+for loquesea in range(3):
+    username = input("Nombre de usuario: ") #Se pide el nombre de usuario
+    password = input("Contrasenia: ") #Se pide una contrasenia
+    unaLista.append(password) #0
+    nombre = input("Ingresa nombre: ")
+    unaLista.append(nombre) #1
         
-apellido = input("Ingresa apellido: ")
-unaLista.append(apellido) #2
+    apellido = input("Ingresa apellido: ")
+    unaLista.append(apellido) #2
        
-edad = input("Ingresa edad: ")
-unaLista.append(edad) #3
+    edad = input("Ingresa edad: ")
+    unaLista.append(edad) #3
+    """       
+    numTarj = input("Ingresa numero de tarjeta: ")
+    unaLista.append(numTarj) #4
         
-numTarj = input("Ingresa numero de tarjeta: ")
-unaLista.append(numTarj) #4
-        
-pp = []
-paypalCorreo = input("Ingresa correo de PayPal: ")
-pp.append(paypalCorreo) #5.0
-paypalContrasenia = input("Ingresa contrasenia de PayPal")
-pp.append(paypalContrasenia) #5.1
+    pp = []
+    paypalCorreo = input("Ingresa correo de PayPal: ")
+    pp.append(paypalCorreo) #5.0
+    paypalContrasenia = input("Ingresa contrasenia de PayPal")
+    pp.append(paypalContrasenia) #5.1
 
-unaLista.append(pp) #5
-usuarios[username] = unaLista
+    unaLista.append(pp) #5 """
+    usuarios[username] = unaLista
 
-llaves =usuarios.keys()
-print("Claves: %s " %llaves)
+#articulo = input(" askk")
+#print(usuarios[articulo])
+
+for elemento in usuarios:
+    print(elemento) #Llave
+    print(usuarios[elemento][0]) #Valor de la lista en la posicion 0
