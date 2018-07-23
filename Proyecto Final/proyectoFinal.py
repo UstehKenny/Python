ef02fd3717d5136344bcdc3d454976df6f9916bb
UstehKenny <carosiim.sdco@gmail.com>
2018-07-22 23:07:37 -0500

    Opcion de correo

--- Proyecto Final/proyectoFinal.py
+++ Proyecto Final/proyectoFinal.py
@@ -1,4 +1,15 @@
 import getpass
+
+# -*- coding: UTF-8 -*-
+
+#Modulos que nos van a permitir enviar correos
+
+import smtplib, getpass, os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
+
 #Dos tipos de usuarios: Compradores y Vendedores
 #Se usaran diccionarios para guardar la informacion de los usuarios y las listas son el valor de cada uno de los diccionarios
 compradores = {}
@@ -9,6 +20,46 @@ listaVendedores = []
 #Se declaran de manera global para poder acceder a la informacion
 articulos = {}
 listaArticulos = []
+
+def enviarCorreo():
+    #Autenticacion
+    user = input("Cuenta gmail: ")
+    password = getpass.getpass("Contraseña: ")
+    #Datos del correo
+    remitente = user
+    destinatario = input("Para, ejemplo <correoGmail>")
+    asunto = input("Asunto: ")
+    mensaje = input("Mensaje: ")
+    archivo = input("Adjuntar archivo: ")
+    #Protocolo y puerto
+    gmail = smtplib.SMTP("smtp.gmail.com",587)
+    #Certificado de seguridad
+    gmail.starttls()
+    #Ingresar
+    gmail.login(user,password)
+    gmail.set_debuglevel(1)
+    #Ingresamos pero falta llenar la informacion
+    header["Subject"]=asunto
+    header["From"]=remitente
+    header["To"]=destinatario
+    #Convertimos a HTML
+    mensaje = MIMEText(mensaje,"html")
+    header.attach(mensaje)
+    #Comprobar que el archivo exista
+    #Comprobando que el archivo exista
+    if(os.path.isfile(archivo)):
+	    adjunto = MIMEBase("application","octet-stream")
+	    adjunto.set_payload(open(archivo,"rb").read())
+	    encode_base64(adjunto)
+	    adjunto.add_header('Content-Disposition', 'attachment; filename="%s"'%os.path.basename(archivo))
+	    header.attach(adjunto)
+
+    #Enviar email
+    gmail.sendmail(remitente,destinatario,header.as_string())
+    #Cerrar sesion
+    gmail.quit()
+
+
 
 def validarContrasenia(passw):
     if len(passw) >= 8:
